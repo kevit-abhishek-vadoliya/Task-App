d35b378c1a3f434aca8870594eea49db41a621f2
kevit-abhishek-vadoliya <kevit.abhishek.vadoliya@gmail.com>
2023-09-27 16:35:18 +0530

    Testing Done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-const express = require('express')
-require("./db/mongoose")
-const userRouters = require('../src/routers/users')
-const taskRouters = require('../src/routers/tasks')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-
-app.use(express.json())
-app.use(userRouters)
-app.use(taskRouters)
-
 
 app.listen(port, () => {
     console.log("server is running on port ", port)
 })
-
-
-
